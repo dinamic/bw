f99915809506892a18bb50f5528b73f181bedd39
Nikola Petkanski <nikola@petkanski.com>
2015-12-29 19:31:00 +0200

    ~ added support for Mac OSX and FreeBSD

--- data.php
+++ data.php
@@ -1,13 +1,18 @@
-<?
+<?php
 
-$int="eth0";
+$interface="en4";
 session_start();
 
-$rx[] = @file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
-$tx[] = @file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
+define('OS_LINUX', 'Linux');
+define('OS_FREEBSD', 'FreeBSD');
+define('OS_OSX', 'Darwin');
+define('PATH_NETSTAT', '/usr/sbin/netstat');
+
+$rx[] = getInterfaceReceivedBytes($interface);
+$tx[] = getInterfaceSentBytes($interface);
 sleep(1);
-$rx[] = @file_get_contents("/sys/class/net/$int/statistics/rx_bytes");
-$tx[] = @file_get_contents("/sys/class/net/$int/statistics/tx_bytes");
+$rx[] = getInterfaceReceivedBytes($interface);
+$tx[] = getInterfaceSentBytes($interface);
 
 $tbps = $tx[1] - $tx[0];
 $rbps = $rx[1] - $rx[0];
@@ -18,20 +23,81 @@ $round_tx=round($tbps/1024, 2);
 $time=date("U")."000";
 $_SESSION['rx'][] = "[$time, $round_rx]";
 $_SESSION['tx'][] = "[$time, $round_tx]";
-$data['label'] = $int;
+
+$data['label'] = $interface;
 $data['data'] = $_SESSION['rx'];
-# to make sure that the graph shows only the
-# last minute (saves some bandwitch to)
+
+/**
+ * to make sure that the graph shows only the
+ * last minute (saves some bandwitch to)
+ */
 if (count($_SESSION['rx'])>60)
 {
     $x = min(array_keys($_SESSION['rx']));
     unset($_SESSION['rx'][$x]);
 }
 
-# json_encode didnt work, if you found a workarround pls write me
-# echo json_encode($data, JSON_FORCE_OBJECT);
+/**
+ * json_encode didnt work, if you found a workarround pls write me
+ * echo json_encode($data, JSON_FORCE_OBJECT);
+ */
 
 echo '
 {"label":"eth0","data":['.implode($_SESSION['rx'], ",").']}
 ';
-?>
+
+function isOSX()
+{
+    $uname = explode(' ', php_uname());
+    return $uname[0] === OS_OSX;
+}
+
+function isLinux()
+{
+    $uname = explode(' ', php_uname());
+    return $uname[0] === OS_LINUX;
+}
+
+function isFreeBSD()
+{
+    $uname = explode(' ', php_uname());
+    return $uname[0] === OS_FREEBSD;
+}
+
+function getInterfaceReceivedBytes($interface)
+{
+    if (isLinux()) {
+        $filepath = '/sys/class/net/%s/statistics/rx_bytes';
+        $output = file_get_contents(sprintf($filepath, $interface));
+
+        return $output;
+    }
+
+    if (isFreeBSD() || isOSX()) {
+        $command = "%s -ibn| grep %s | grep Link | awk '{print $7}'";
+        exec(sprintf($command, PATH_NETSTAT, $interface), $output);
+
+        return array_shift($output);
+    }
+
+    throw new Exception('Unable to guess OS');
+}
+
+function getInterfaceSentBytes($interface)
+{
+    if (isLinux()) {
+        $filepath = '/sys/class/net/%s/statistics/tx_bytes';
+        $output = file_get_contents(sprintf($filepath, $interface));
+
+        return $output;
+    }
+
+    if (isFreeBSD() || isOSX()) {
+        $command = "%s -ibn| grep %s | grep Link | awk '{print $10}'";
+        exec(sprintf($command, PATH_NETSTAT, $interface), $output);
+
+        return array_shift($output);
+    }
+
+    throw new Exception('Unable to guess OS');
+}
